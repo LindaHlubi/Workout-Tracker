1ac3f2d3768a216d41ea42c4958e4fb6aa2508dc
LINDA_HLUBI <linda_hlubi@icloud.com>
2021-04-27 22:16:41 +1000

    api routes get workout

---
+++ routes/apiRoutes.js
@@ -0,0 +1,20 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+const mongojs = require("mongodb");
+
+//get workout
+router.get("/api/workouts", (req,res) => {
+    Workout.find({})
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+})
+
+
+
+
+
+module.exports = router;
