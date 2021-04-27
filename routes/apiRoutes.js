725cf2af04c6787c1e5c61cc44b9647a3dfa7bd7
LINDA_HLUBI <linda_hlubi@icloud.com>
2021-04-27 22:42:41 +1000

    router put and post api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -11,7 +11,27 @@ router.get("/api/workouts", (req,res) => {
       .catch(err => {
         res.status(400).json(err);
       });
-})
+});
+
+      // add and update exercise
+      router.put("/api/workouts/:id", (req, res) => {
+        Workout.findByIdAndUpdate(
+          { _id: req.params.id }, { exercises: req.body }
+        ).then((dbWorkout) => {
+          res.json(dbWorkout);
+        }).catch(err => {
+          res.status(400).json(err);
+        });
+    });
+
+    //create a workout
+    router.post("/api/workouts/", (req, res) => {
+      Workout.create(req.body).then((dbWorkout) => {
+        res.json(dbWorkout);
+      }).catch(err => {
+          res.status(400).json(err);
+        });
+    });
 
 
 
