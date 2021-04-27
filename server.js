8657bb46f9836eb2a84abc5273047760761885a4
LINDA_HLUBI <linda_hlubi@icloud.com>
2021-04-27 22:02:06 +1000

    commit server.js

---
+++ server.js
@@ -0,0 +1,34 @@
+//require dependencies
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+});
+
+//Routes
+require("./routes/htmlRoutes")(app);
+require("./routes/apiRoutes")(app);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, () => {
+    // Log (server-side) when our server has started
+  console.log(`App running on port ${PORT}!`);
+});
